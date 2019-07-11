63dc30935eaeb20c5313f72b8eef37f0d1242fb1
ragha <vamshimidjilla@gmail.com>
2019-07-12 00:30:00 +0530

    Updated login page from tests to pages

--- ypoconnect/src/main/java/com/ypo/pages/Loginpage.java
+++ ypoconnect/src/main/java/com/ypo/pages/Loginpage.java
@@ -1,5 +1,84 @@
 package com.ypo.pages;
 
-public class Loginpage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class Loginpage 
+
+{
+	WebDriver driver;
+
+	
+	public Loginpage(WebDriver rdriver)
+	{
+		driver=rdriver;
+		PageFactory.initElements(rdriver, this);
+	}
+		
+	@FindBy(xpath="//input[@id='username_input']")
+	WebElement username;
+
+	@FindBy(xpath="//input[@id='password_input']")
+	WebElement password;
+	
+	@FindBy(xpath="//button[@id='submit_login_btn']")
+	WebElement submit;
+	
+	@FindBy(xpath="i[@id='username_assistance_icon']")
+	WebElement usrnameassist;
+	
+	
+	@FindBy(xpath="i[@id='password_assistance_icon']")
+	WebElement passwrdassist;
+	
+	@FindBy(xpath="button[@class='text-capitalize v-btn v-btn--flat v-btn--small theme--light']/div")
+	WebElement privacy;
+	
+	@FindBy(xpath="login-card__termsofuse v-btn v-btn--flat v-btn--small theme--light")
+	WebElement termsofuse;
+	
+	//@FindBy(xpath="button[@class='text-capitalize v-btn v-btn--flat v-btn--small theme--light']/div")
+	//WebElement contactus;
+	
+	
+	////button[@class='text-capitalize v-btn v-btn--flat v-btn--small theme--light']/div
+
+
+public void setusername(String name)
+{
+	username.sendKeys(name);
+	
+}
+
+public void setpassword(String passwrd)
+{
+password.sendKeys(passwrd);
+	
+}
+
+public void click_login()
+{
+	submit.click();
+	
+}
+
+public void clickonasstusrname(String name)
+{
+	usrnameassist.click();
 
 }
+
+public void clickprivacy()
+{
+	privacy.click();
+
+}
+
+}
+
+	
+
+
+
